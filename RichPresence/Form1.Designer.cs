a4bd18e51d2c4fe0597052a59d33ef678686a354
Raffaele Gigantelli <84672573+Warmzzz@users.noreply.github.com>
2021-10-16 10:31:18 +0200

    Add files via upload

---
+++ RichPresence/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿
+namespace RichPresence
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbStartTimestamp = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbEndTimestamp = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbClientID = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbDetails = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbState = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbImage = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.ouvrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(5, 422);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(538, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Changer la RichPresence";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(157, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(261, 88);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(1, 243);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Début Horodatage :";
+            // 
+            // tbStartTimestamp
+            // 
+            this.tbStartTimestamp.Location = new System.Drawing.Point(157, 245);
+            this.tbStartTimestamp.Name = "tbStartTimestamp";
+            this.tbStartTimestamp.Size = new System.Drawing.Size(184, 20);
+            this.tbStartTimestamp.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 271);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Fin Horodatage :";
+            // 
+            // tbEndTimestamp
+            // 
+            this.tbEndTimestamp.Location = new System.Drawing.Point(157, 271);
+            this.tbEndTimestamp.Name = "tbEndTimestamp";
+            this.tbEndTimestamp.Size = new System.Drawing.Size(184, 20);
+            this.tbEndTimestamp.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(77, 313);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Client ID:";
+            // 
+            // tbClientID
+            // 
+            this.tbClientID.Location = new System.Drawing.Point(157, 313);
+            this.tbClientID.Name = "tbClientID";
+            this.tbClientID.Size = new System.Drawing.Size(184, 20);
+            this.tbClientID.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(85, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Details :";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // tbDetails
+            // 
+            this.tbDetails.Location = new System.Drawing.Point(157, 171);
+            this.tbDetails.Name = "tbDetails";
+            this.tbDetails.Size = new System.Drawing.Size(386, 20);
+            this.tbDetails.TabIndex = 6;
+            this.tbDetails.TextChanged += new System.EventHandler(this.tbDetails_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(95, 197);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "State :";
+            this.label5.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // tbState
+            // 
+            this.tbState.Location = new System.Drawing.Point(157, 197);
+            this.tbState.Name = "tbState";
+            this.tbState.Size = new System.Drawing.Size(386, 20);
+            this.tbState.TabIndex = 6;
+            this.tbState.TextChanged += new System.EventHandler(this.tbDetails_TextChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(157, 389);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(162, 24);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Afficher les images";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(26, 363);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(125, 20);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Nom de l\'image :";
+            // 
+            // tbImage
+            // 
+            this.tbImage.Location = new System.Drawing.Point(157, 363);
+            this.tbImage.Name = "tbImage";
+            this.tbImage.Size = new System.Drawing.Size(184, 20);
+            this.tbImage.TabIndex = 3;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(549, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripSeparator1,
+            this.ouvrirToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.toolStripMenuItem2});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem1.Text = "Sauvegarder";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // ouvrirToolStripMenuItem
+            // 
+            this.ouvrirToolStripMenuItem.Name = "ouvrirToolStripMenuItem";
+            this.ouvrirToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.ouvrirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ouvrirToolStripMenuItem.Text = "Ouvrir";
+            this.ouvrirToolStripMenuItem.Click += new System.EventHandler(this.ouvrirToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(177, 6);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Q)));
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem2.Text = "Quitter";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 466);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.tbState);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbDetails);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbImage);
+            this.Controls.Add(this.tbClientID);
+            this.Controls.Add(this.tbEndTimestamp);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbStartTimestamp);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(565, 505);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(565, 505);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RichPresence-App by Warmzz";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbStartTimestamp;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbEndTimestamp;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbClientID;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbDetails;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbState;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbImage;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem ouvrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.BindingSource bindingSource1;
+    }
+}
+
---
+++ RichPresence/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DiscordRpcDemo;
+
+namespace RichPresence
+{
+    public partial class Form1 : Form
+    {
+        public NotifyIcon notifyIcon;
+        public BackgroundWorker bw;
+        public DiscordRpc.EventHandlers handlers;
+        public DiscordRpc.RichPresence presence;
+        public string cbChecked;
+        public SaveFileDialog SFD;
+        string pathSave;
+        public OpenFileDialog OFD;
+        string pathOpen;
+        int endNum;
+        int startNum;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bw = new BackgroundWorker();
+            bw.DoWork += (obs, ea) => RpcDiscordChange();
+            bw.RunWorkerAsync();
+
+            this.Hide();
+
+            notifyIcon = new NotifyIcon();
+            notifyIcon.Icon = new System.Drawing.Icon("discorde.ico");
+            notifyIcon.Text = "RichPresence-App";
+
+            notifyIcon.ContextMenuStrip = new ContextMenuStrip();
+            notifyIcon.ContextMenuStrip.Items.Add("Ouvrir", Image.FromFile("discorde.ico"), IconMenuOpen);
+            notifyIcon.ContextMenuStrip.Items.Add("Quitter", Image.FromFile("discorde.ico"), IconMenuQuit);
+
+            notifyIcon.Visible = true;
+        }
+
+        private void IconMenuQuit(object sender, EventArgs e)
+        {
+            Application.Exit();
+            this.Close();
+        }
+
+        private void IconMenuOpen(object sender, EventArgs e)
+        {
+            this.Show();
+            notifyIcon.Visible = true;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbDetails_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RpcDiscordChange()
+        {
+            try
+            {
+                this.handlers = default(DiscordRpc.EventHandlers);
+                if (String.IsNullOrWhiteSpace(tbClientID.Text))
+                {
+                    DiscordRpc.Initialize("897850326286221363", ref this.handlers, true, null);
+                }
+                else
+                {
+                    DiscordRpc.Initialize(tbClientID.Text, ref this.handlers, true, null);
+                }
+
+                if (!String.IsNullOrWhiteSpace(tbDetails.Text))
+                {
+                    this.presence.details = tbDetails.Text;
+                }
+
+                if (!String.IsNullOrWhiteSpace(tbState.Text))
+                {
+                    this.presence.state = tbState.Text;
+                }
+
+                if (!String.IsNullOrWhiteSpace(tbStartTimestamp.Text))
+                {
+                    try{ startNum = int.Parse(tbStartTimestamp.Text); }catch{}
+                    this.presence.startTimestamp = startNum;
+                }
+
+                if (!String.IsNullOrWhiteSpace(tbEndTimestamp.Text))
+                {
+                    try { endNum = int.Parse(tbEndTimestamp.Text); }catch{}
+                    this.presence.endTimestamp = endNum;
+                }
+
+                if (checkBox1.Checked)
+                {
+                    if (String.IsNullOrWhiteSpace(tbImage.Text))
+                    {
+                        this.presence.largeImageKey = tbImage.Text;
+                        this.presence.largeImageText = tbImage.Text;
+                    }
+                    else
+                    {
+                        this.presence.largeImageKey = "discorde";
+                        this.presence.largeImageText = "RichPresence-App";
+                    }
+                }
+
+                DiscordRpc.UpdatePresence(ref this.presence);
+                MessageBox.Show(text:"Votre Rpc Discord a été changé avec succès !",caption:"Tout va bien", buttons: MessageBoxButtons.OK);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(text:$"Une erreur s'est produite lors du changement de votre Rpc Discord !\n\n{e}", caption: "Une erreur s'est produite", buttons:MessageBoxButtons.OK, icon:MessageBoxIcon.Error);
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if (checkBox1.Checked)
+                {
+                    cbChecked = "True";
+                }
+                else
+                {
+                    cbChecked = "False";
+                }
+
+                string textSave =
+                    $"{tbDetails.Text}|{tbState.Text}|{tbStartTimestamp.Text}|{tbEndTimestamp.Text}|{tbClientID.Text}|{tbImage.Text}|{cbChecked}";
+                SFD = new SaveFileDialog();
+                SFD.FileName = "RichPresenceApp.txt";
+                SFD.Filter = "Fichier texte (*.txt)|*.txt";
+                DialogResult dr = SFD.ShowDialog();
+                if (dr == DialogResult.OK)
+                {
+                    pathSave = SFD.FileName;
+                }
+
+                File.WriteAllText(pathSave, textSave);
+            }
+            catch
+            {
+                MessageBox.Show(text: "Une erreur s'est produite lors de la sauvegarde du fichier !", caption: "Une erreur s'est produite", buttons: MessageBoxButtons.OK, icon: MessageBoxIcon.Error);
+            }
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Application.Exit();
+                this.Close();
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        private void ouvrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string[] textOpen;
+                OFD = new OpenFileDialog();
+                OFD.Filter = "Fichier texte (*.txt)|*.txt";
+                DialogResult dialogResult = OFD.ShowDialog();
+                if(dialogResult == DialogResult.OK){pathOpen = OFD.FileName;}
+                string textO = File.ReadAllText(pathOpen);
+                textOpen = textO.Split('|');
+                tbDetails.Text = textOpen[0];
+                tbState.Text = textOpen[1];
+                tbStartTimestamp.Text = textOpen[2];
+                tbEndTimestamp.Text = textOpen[3];
+                tbClientID.Text = textOpen[4];
+                tbImage.Text = textOpen[5];
+                if (textOpen[6] == "True")
+                {
+                    checkBox1.Checked = true;
+                }
+                else
+                {
+                    checkBox1.Checked = false;
+                }
+            }
+            catch
+            {
+                MessageBox.Show(text: "Une erreur s'est produite lors de l'ouverture de fichier de sauvegarde !", caption: "Une erreur s'est produite", buttons: MessageBoxButtons.OK, icon: MessageBoxIcon.Error);
+            }
+        }
+    }
+}
